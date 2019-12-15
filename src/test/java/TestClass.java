7815a8d456e46036663f7e6837ccd449112229cf
Yuliya Gerashchenko <merg@list.ru>
2019-12-15 17:44:43 +0300

    last commit

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -7,19 +7,19 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 class TestClass {
-//     @Test
-//     void TestCardForm() throws InterruptedException {
-//         open("http://localhost:9999");
-//         SelenideElement form = $("form");
-//         form.$("[data-test-id=name] input").setValue("Иван Ивановтч");
-//         form.$("[data-test-id=phone] input").setValue("+79947013020");
-//         form.$("[data-test-id=agreement]").click();
-//         form.$("[class=\"button button_view_extra button_size_m " +
-//                 "button_theme_alfa-on-white\"]").click();
-//         $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! " +
-//                 "Наш менеджер свяжется с вами в ближайшее время."));
-//         Thread.sleep(5000);
-//     }
+     @Test
+     void TestCardForm() throws InterruptedException {
+         open("http://localhost:9999");
+         SelenideElement form = $("form");
+         form.$("[data-test-id=name] input").setValue("Иван Ивановтч");
+         form.$("[data-test-id=phone] input").setValue("+79947013020");
+         form.$("[data-test-id=agreement]").click();
+         form.$("[class=\"button button_view_extra button_size_m " +
+                 "button_theme_alfa-on-white\"]").click();
+         $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! " +
+                 "Наш менеджер свяжется с вами в ближайшее время."));
+         Thread.sleep(5000);
+     }
 
 
      @Test
